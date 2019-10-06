b7dddec09bb0bbde2fba63bcbc79c083982eb06c
joyoverflow <joyoverflow@sina.com>
2019-10-06 23:58:40 +0800

    添加JsonObject解析

--- JobCenter/src/main/java/ouyj/hyena/com/jobcenter/MainActivity.java
+++ JobCenter/src/main/java/ouyj/hyena/com/jobcenter/MainActivity.java
@@ -3,6 +3,9 @@ package ouyj.hyena.com.jobcenter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //创建JsonObject对象
+        JSONObject users = new JSONObject();
+        try {
+            users.put("name","ouyj1");
+            users.put("age", 42);
+            users.put("gender", true);
+            users.put("salary", 25000);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
